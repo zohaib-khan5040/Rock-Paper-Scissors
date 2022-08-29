fe4af101eecc49b016bb0addec5df70c3f0c4e50
zohaib-khan5040 <zohaib.khan3502@gmail.com>
2022-08-29 17:02:21 +0500

    Add UI for playing 5 rounds

--- app.js
+++ app.js
@@ -46,6 +46,34 @@ function game() {
     console.log(result);
 }
 
-for (let i=0; i<5; i++) {
-    game();
-}
+let gamesWon = 0;
+let gamesPlayed = 0;
+const scoreDisplay = document.querySelector('#score');
+
+// Get the three buttons for player choice and add click listeners
+const buttons = document.querySelectorAll(".selection");
+buttons.forEach((button) => {
+    button.addEventListener("click", function (e) {
+
+        if (gamesPlayed == 5) {
+            let winnerStatement = gamesWon >= 3 ? "You are" : "You are not";
+            alert(`Match Finished! ${winnerStatement} the Winner!`);
+            return;
+        }
+        
+        let ps = button.innerText;
+        let cs = getComputerChoice();
+
+        console.log(`Player clicked ${ps}`);
+
+        // Play a round of RPS
+        let result = playRound(ps, cs);
+        gamesPlayed++;
+        alert(result);
+        
+        if (result.startsWith("You Win")) {
+            scoreDisplay.innerText = `${++gamesWon}/5`;
+        }
+
+    })
+})
